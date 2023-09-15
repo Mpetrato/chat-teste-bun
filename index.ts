78bd4c1d2a23890bba10c4d6d49b99b7d2f79089
matheus <m.rocha@vertex.vertexdigital.co>
2023-09-15 20:51:51 -0300

    testando

---
+++ index.ts
@@ -0,0 +1,52 @@
+let uuid = ""
+
+Bun.serve({
+  port: 3000,
+  fetch(req, server) {
+    const myURL = new URL(req.url);
+    if(!/favicon/.test(myURL.pathname)) {
+      uuid = myURL.pathname
+    }
+    return new Response(Bun.file("./index.html"));
+  },
+});
+
+const messages: any = [];
+let users: any = [];
+
+Bun.serve({
+  port: 4000,
+  fetch(req, server) {
+    const success = server.upgrade(req, {
+      data: { username: "user_" + Math.random().toString(16).slice(12) },
+    });
+
+    return success
+      ? undefined
+      : new Response("Upgrade failed :(", { status: 500 });
+  },
+  websocket: {
+    open(ws) {
+      ws.subscribe("chat" + uuid);
+    },
+    message(ws, data) {
+      const message = JSON.parse(data);
+      message.username = ws.data.username;
+      messages.push(message);
+
+      ws.publish(
+        `chat${uuid}`,
+        JSON.stringify({ type: "MESSAGES_ADD", data: message })
+      );
+    },
+    close(ws) {
+      users = users.filter((username: any) => username !== ws.data.username);
+
+      // Send message to all clients subscribed to the chat channel that user left
+      ws.publish(
+        "chat"+ uuid,
+        JSON.stringify({ type: "USERS_REMOVE", data: ws.data.username })
+      );
+    },
+  },
+});
